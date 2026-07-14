2359d172b0efb18a3835d2f4e1fec8093b63e585
J-Mohamed-Fazil <mf20159126@gmail.com>
2026-07-14 20:40:44 +0530

    Initial commit - Created Database

---
+++ bankingdatabase.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE  BankingDB;
+USE BankingDB;
+
+CREATE TABLE Customers(
+	CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+
+DESCRIBE Customers;
